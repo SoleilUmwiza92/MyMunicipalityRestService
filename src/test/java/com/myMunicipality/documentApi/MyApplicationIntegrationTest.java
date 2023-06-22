a4055ab82d7c489406705ff1142fcb22094f2ffa
SoleilUmwiza92 <soleilmwiza@gmail.com>
2023-06-22 02:32:02 +0200

    Updating integration and unit testing

--- src/test/java/com/myMunicipality/documentApi/MyApplicationIntegrationTest.java
+++ src/test/java/com/myMunicipality/documentApi/MyApplicationIntegrationTest.java
@@ -1,22 +1,30 @@
-package com.myMunicipality.documentApi.repository;
+package com.myMunicipality.documentApi;
 
 import com.myMunicipality.documentApi.model.Document;
+import com.myMunicipality.documentApi.repository.DocumentRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mockStatic;
 
-@SpringBootTest
-class DocumentRepositoryIntegrationTest {
+@DataMongoTest
+public class MyApplicationIntegrationTest {
 
     @Autowired
     private MongoTemplate mongoTemplate;
     @Autowired
     DocumentRepository repository;
+        @Test
+        public void main() {
+            mockStatic(SpringApplication.class);
+            DocumentApiApplication.main(new String[] {});
+        }
 
     @Test
     void bootstrapTestDataWithMongoTemplate() {
@@ -31,7 +39,7 @@ class DocumentRepositoryIntegrationTest {
         mongoTemplate.insert(doc1);
         var docs= repository.findAll();
         assertNotNull(docs);
-        assertEquals(docs.stream().findFirst().get(), "id1");
+        assertEquals(docs.stream().findFirst().get().getId(), "id1");
     }
     @Test
     void findByIdTest(){
@@ -44,7 +52,7 @@ class DocumentRepositoryIntegrationTest {
 
     @AfterEach
     void cleanUpDatabase() {
-       repository.findAll().forEach(doc -> mongoTemplate.remove(doc));
+        repository.findAll().forEach(doc -> mongoTemplate.remove(doc));
 
     }
 }
